78549f1bedb02f2921b254902456c88f26cceefb
Matheqs <matheqs@outlook.com>
2025-09-02 18:50:37 -0300

    Models e diretório DATA

---
+++ Models/Livro.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Models
+{
+	public class Livro
+	{
+		public int Id { get; set; }
+		public string Autor { get; set; }
+		public string Titulo { get; set; }
+		public string Editora { get; set; }
+		public int Ano { get; set; }
+
+	}
+}
